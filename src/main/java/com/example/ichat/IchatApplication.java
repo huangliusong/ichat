fb04925a502fb9363e9e9ed3e339cbe01dfefb1d
liusonghuang <huangliusong1994@gmail.com>
2019-05-03 07:06:47 +0800

    Initial commit ichat

---
+++ src/main/java/com/example/ichat/IchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.ichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IchatApplication.class, args);
+    }
+
+}
