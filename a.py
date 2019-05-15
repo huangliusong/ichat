646eee1bb1e56d278d91d8aca4103aaf790f2920
huangliusong <41847999+huangliusong1994@users.noreply.github.com>
2019-05-15 10:06:28 +0800

    def ReadTxtName(rootdir):     lines = []     with open(rootdir, 'r') as file_to_read:         while True:             line = file_to_read.readline()             if not line:                 break             line = line.strip('\n')             lines.append(line)     return lines   resultpath='/Users/liusonghuang/Desktop/a.txt' lineslist=ReadTxtName(resultpath) for x in lineslist:     print('list:',x)
    def ReadTxtName(rootdir):
        lines = []
        with open(rootdir, 'r') as file_to_read:
            while True:
                line = file_to_read.readline()
                if not line:
                    break
                line = line.strip('\n')
                lines.append(line)
        return lines
    
    
    resultpath='/Users/liusonghuang/Desktop/a.txt'
    lineslist=ReadTxtName(resultpath)
    for x in lineslist:
        print('list:',x)

---
+++ a.py
@@ -0,0 +1,13 @@
+def ReadTxtName(rootdir):
+    lines = []
+    with open(rootdir, 'r') as file_to_read:
+        while True:
+            line = file_to_read.readline()
+            if not line:
+                break
+            line = line.strip('\n')
+            lines.append(line)
+    return lines
+
+
+ resultpath='/Users/liusonghuang/Desktop/a.txt'
---
+++ t.py
@@ -0,0 +1,16 @@
+def ReadTxtName(rootdir):
+    lines = []
+    with open(rootdir, 'r') as file_to_read:
+        while True:
+            line = file_to_read.readline()
+            if not line:
+                break
+            line = line.strip('\n')
+            lines.append(line)
+    return lines
+
+
+resultpath='/Users/liusonghuang/Desktop/a.txt'
+lineslist=ReadTxtName(resultpath)
+for x in lineslist:
+    print('list:',x)
